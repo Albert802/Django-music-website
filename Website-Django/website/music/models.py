a445b728bcb54e419287a0232040c7f93634e308
Albert802 <92227000+Albert802@users.noreply.github.com>
2021-11-21 20:41:39 +0000

    Add files via upload

---
+++ Website-Django/website/music/models.py
@@ -0,0 +1,29 @@
+
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Album(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
+    is_favorite = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.song_title
+
